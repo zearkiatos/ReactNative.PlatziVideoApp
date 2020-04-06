0aa4c6c0fb50168631015fbb5801a31d6e54beb5
Pedro Capriles <caprilespe@outlook.com>
2020-04-05 23:12:45 -0400

    Creation Header

--- App.js
+++ App.js
@@ -1,12 +1,13 @@
 import React, {Component} from 'react';
 import {Text} from 'react-native';
 import Home from './src/screens/containers/home';
+import Header from './src/sections/components/header';
 type Props = {};
 export default class App extends Component<Props> {
   render() {
     return (
       <Home>
-        <Text>Header</Text>
+        <Header />
         <Text>Buscador</Text>
         <Text>Categorías</Text>
         <Text>Sugerencias</Text>
