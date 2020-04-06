0727970ef6fc3b6669f7aabfde02bad15a0ed878
Pedro Capriles <caprilespe@outlook.com>
2020-04-05 23:42:05 -0400

    Recomendation Structure

--- src/videos/containers/suggestionList.js
+++ src/videos/containers/suggestionList.js
@@ -1,22 +1,26 @@
 import React, {Component} from 'react';
 import {FlatList, Text} from 'react-native';
+import Layout from '../components/suggestionListLayout';
+
 class SuggestionList extends Component {
   render() {
     const list = [
       {
-        title: 'Pedro',
+        title: 'titulo',
         key: '1',
       },
       {
-        title: 'Capriles',
+        title: 'otro titulo',
         key: '2',
       },
     ];
     return (
-      <FlatList
-        data={list}
-        renderItem={({item}) => <Text>{item.title}</Text>}
-      />
+      <Layout title="Recomendado para ti">
+        <FlatList
+          data={list}
+          renderItem={({item}) => <Text>{item.title}</Text>}
+        />
+      </Layout>
     );
   }
 }
