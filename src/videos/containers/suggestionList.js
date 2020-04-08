84d55566416bd4637e6d24df36687de06d0b1d6f
Pedro Capriles <caprilespe@outlook.com>
2020-04-07 21:27:33 -0400

    Separate empty Lists

--- src/videos/containers/suggestionList.js
+++ src/videos/containers/suggestionList.js
@@ -1,23 +1,28 @@
 import React, {Component} from 'react';
 import {FlatList, Text} from 'react-native';
 import Layout from '../components/suggestionListLayout';
-
+import Empty from '../components/empty';
+import Separator from '../components/verticalSeparator';
 class SuggestionList extends Component {
+  renderEmpty = () => <Empty text="No hay elementos sugeridos ☹" />;
+  itemSeparator = () => <Separator />;
   render() {
     const list = [
       {
-        title: 'titulo',
-        key: '1',
+        title: 'Avengers',
+        key: 1,
       },
       {
-        title: 'otro titulo',
-        key: '2',
+        title: 'Pokemon',
+        key: 2,
       },
     ];
     return (
       <Layout title="Recomendado para ti">
         <FlatList
           data={list}
+          ListEmptyComponent={this.renderEmpty}
+          ItemSeparatorComponent={this.itemSeparator}
           renderItem={({item}) => <Text>{item.title}</Text>}
         />
       </Layout>
